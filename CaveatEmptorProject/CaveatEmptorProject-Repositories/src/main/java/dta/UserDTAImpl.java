69f9ac91cef327b0b7151f10576f09c9845281b7
madalina.palade <madalina.palade@WS-IS-U3-3359.ad.fortech.ro>
2017-05-19 17:27:40 +0300

    File added

--- CaveatEmptorProject/CaveatEmptorProject-Repositories/src/main/java/dta/UserDTAImpl.java
+++ CaveatEmptorProject/CaveatEmptorProject-Repositories/src/main/java/dta/UserDTAImpl.java
@@ -1,6 +1,5 @@
 package dta;
 
-
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
@@ -21,85 +20,82 @@ public class UserDTAImpl implements UserDTA {
 	public Users findUserByUsername(String username) throws CaveatEmptorException {
 		Users user = new Users();
 		try {
-			 user = (Users) entityManager.createNamedQuery(Constants.FIND_USER_BY_USERNAME)
+			user = (Users) entityManager.createNamedQuery(Constants.FIND_USER_BY_USERNAME)
 					.setParameter("username", username).getSingleResult();
-			 
-				 return user;
+
+			return user;
 		} catch (Exception ex) {
-					throw new CaveatEmptorException(
-							"Exception into findUserByUsername method for username= " + username +"-> "+ ex.getMessage());
+			throw new CaveatEmptorException(
+					"Exception into findUserByUsername method for username= " + username + "-> " + ex.getMessage());
 		}
 	}
-	
+
 	@Override
 	public Users findUserByEmail(String email) throws CaveatEmptorException {
 		Users user = new Users();
 		try {
-			 user = (Users) entityManager.createNamedQuery(Constants.FIND_USER_BY_EMAIL)
-					.setParameter("email", email).getSingleResult();
+			user = (Users) entityManager.createNamedQuery(Constants.FIND_USER_BY_EMAIL).setParameter("email", email)
+					.getSingleResult();
 			return user;
 
 		} catch (Exception ex) {
-					throw new CaveatEmptorException(
-							"Exception into findUserByUsername method for username= " + email +"-> "+ ex.getMessage());
+			throw new CaveatEmptorException(
+					"Exception into findUserByUsername method for username= " + email + "-> " + ex.getMessage());
 		}
 	}
-	
-	public boolean existUser(String option,String value) throws CaveatEmptorException {
-		
-		Users user=new Users();
-		
-		if(option.equals("username")){
-			user=findUserByUsername(value);
-			if(user!=null)
+
+	public boolean existUser(String option, String value) throws CaveatEmptorException {
+
+		Users user = new Users();
+
+		if (option.equals("username")) {
+			user = findUserByUsername(value);
+			if (user != null)
 				return true;
 		}
-		if(option.equals("email")){
-			user=findUserByEmail(value);
-			if(user!=null)
+		if (option.equals("email")) {
+			user = findUserByEmail(value);
+			if (user != null)
 				return true;
 		}
 		return false;
 	}
-	
+
 	@Override
-	public String createUser(UserDTO userDto) throws CaveatEmptorException
-	{
-		Users user=new Users();
-		
+	public String createUser(UserDTO userDto) throws CaveatEmptorException {
+		Users user = new Users();
+
 		user.setFirstname(userDto.getFirstname());
 		user.setLastname(userDto.getLastname());
 		user.setEmail(userDto.getEmail());
 		user.setPassword(userDto.getPassword());
 		user.setPhoneNumber(userDto.getPhoneNumber());
 		user.setUsername(userDto.getUsername());
-		
-		boolean usernameExist=false;
-		boolean emailExist=false;
-		
-		
-		try{
-			usernameExist=existUser("username",userDto.getUsername());
-		}catch(CaveatEmptorException ex){
+
+		boolean usernameExist = false;
+		boolean emailExist = false;
+
+		try {
+			usernameExist = existUser("username", userDto.getUsername());
+		} catch (CaveatEmptorException ex) {
 			ex.getMessage();
 		}
 
-		
-		try{
-			emailExist=existUser("email",userDto.getEmail());
-		}catch(CaveatEmptorException ex){
+		try {
+			emailExist = existUser("email", userDto.getEmail());
+		} catch (CaveatEmptorException ex) {
 			ex.getMessage();
 		}
-		
-		if(emailExist)
+
+		if (emailExist)
 			return "emailExist";
-		else if(usernameExist)
+		else if (usernameExist)
 			return "usernameExist";
-		else{
+		else {
 			entityManager.persist(user);
 			return "userCreated";
 		}
-		
+
 	}
 
 }
